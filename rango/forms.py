919fed577568c125b607e5c251b33a820db81319
suwenkun1126 <suwenkun1126@126.com>
2018-03-29 20:30:24 +0800

    集成Bootstrap

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Category,Page,UserProfile
 
 class CategoryForm(forms.ModelForm):
-    name=forms.CharField(max_length=128,help_text="Please enter the category name")
+    name=forms.CharField(max_length=128,help_text="分类名称")
     views=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     likes=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     slug=forms.CharField(widget=forms.HiddenInput(),required=False)
@@ -13,8 +13,8 @@ class CategoryForm(forms.ModelForm):
         fields=('name',)
 
 class PageForm(forms.ModelForm):
-    title=forms.CharField(max_length=128,help_text="Please enter the title of the page")
-    url=forms.URLField(max_length=200,help_text="Please enter the URL of the page")
+    title=forms.CharField(max_length=128,help_text="网页名称")
+    url=forms.URLField(max_length=200,help_text="网页链接")
     views=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
 
     class Meta:
